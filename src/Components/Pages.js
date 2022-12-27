92ec0a55c9320a188a8de27e63324774d49c8463
Sumair10 <sumairzahid123@gmail.com>
2022-12-27 12:50:49 +0500

    model

---
+++ src/Components/Pages.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import MMC from './MMC';
+
+export default function LabTabs() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="M/M/C" value="1" />
+            <Tab label="M/M/Inf" value="2" />
+            <Tab label="M/M/C/K" value="3" />
+            <Tab label="M/M/C/*/M" value="4" />
+          </TabList>
+        </Box>
+        <TabPanel value="1"><MMC/></TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="4">Item Three</TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
