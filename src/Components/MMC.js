549640468da309be636a09c75ddddb40ab14e7ab
Sumair10 <sumairzahid123@gmail.com>
2023-01-03 17:03:46 +0500

    MMC

--- src/Components/MMC.js
+++ src/Components/MMC.js
@@ -40,7 +40,7 @@ export default function MMC() {
     });
     setL(lemda / (mue - lemda));
     setLq((lemda * lemda) / (mue / (mue - lemda)));
-    // setW(1 / (mue - lemda));
+    setW(1 / (mue - lemda));
     // setWq(lemda / (mue * (mue - lemda)));
   };
 
