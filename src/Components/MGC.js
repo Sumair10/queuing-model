501a028fba738dee2fbd053f318cef141dbc0d4e
Sumair10 <sumairzahid123@gmail.com>
2023-01-15 00:46:26 +0500

    MMC

--- src/Components/MGC.js
+++ src/Components/MGC.js
@@ -87,7 +87,7 @@ export default function MGC() {
       var listInSystem = lemda * waitInSystem;
       var idelServerProb = (1 - ro) * 100;
 
-      setLq(listInQueue);
+      // setLq(listInQueue);
       setWq(waitInQueue);
       setW(waitInSystem);
       setL(listInSystem);
